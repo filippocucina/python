a7cde9d5221647bbcb1261969ca01cc8a671cee1
Filippo <filippocucina5@gmail.com>
2023-09-24 20:32:34 -0400

    Funciones matematicas en Python

---
+++ for_loops/mathematics.py
@@ -0,0 +1,13 @@
+import sys, math
+
+print(math.ceil(1.4))
+print(math.ceil(3.8))
+print(math.ceil(5.2))
+print(math.ceil(7))
+print(math.ceil(9.1))
+
+#Binomial Theorem
+print(math.comb(5, 4))
+print(math.comb(3, 5)) #Evaluates to zero when k > n  math.comb(n , k)
+print(math.comb(3, 2))
+print(math.fabs(-30))
