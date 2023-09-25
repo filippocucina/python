9914b9560fbdb69504d981ab28e8b37f2011bed6
Filippo <filippocucina5@gmail.com>
2023-09-24 20:35:15 -0400

    Argumentos por linea de comandos

---
+++ for_loops/command_line_argument_parse.py
@@ -0,0 +1,8 @@
+import argparse
+
+parser = argparse.ArgumentParser(prog = 'Command_line_argument_parser.py',
+                                 usage = 'Practicing Command Lines Arguments',
+                                 description = 'What the program does',
+                                 epilog = 'Text at the bottom of help' )
+
+print(parser)
