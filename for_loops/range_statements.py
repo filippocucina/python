48b6814c5004e24a8ab8e0b63286a53fcdfffd4c
Filippo <filippocucina5@gmail.com>
2023-09-24 20:34:16 -0400

    Ciclos for rango

---
+++ for_loops/range_statements.py
@@ -0,0 +1,14 @@
+# for loop instruction
+# for i in range(x, y, step)
+
+for i in range(5):
+    print(f"{i}")
+
+for j in range(5, 10):
+    print(f"{j}")
+
+for k in range(5, 15, 3):
+    print(f"{k}")
+
+for l in range(15, 5, -3):
+    print(f"{l}")
